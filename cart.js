eb02b54a2a1f53fd2ad11ad7c975a5723ed3ba41
alannahkotvas <alannah.kotvas@student.unimelb.edu.au>
2024-05-29 20:09:39 +1000

    REMOVE ITEM FROM CART WORKING WOOO

--- cart.js
+++ cart.js
@@ -1,1 +1,32 @@
-console.log("hi :)");
+
+//makes sure remove item from cart removes the entire row 
+
+var removeCartItemButtons = document.getElementsByClassName('btn-danger') //stores all buttons w the class btn-danger
+console.log(removeCartItemButtons);
+
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+
+//changes total cost after items have been changed/ removed
+
+function updateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')
+        [0]
+        var price = parseFloat(priceElement.innerText.replace('$',''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
